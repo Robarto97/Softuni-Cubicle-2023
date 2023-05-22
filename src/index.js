40b53bc47cd9fbbb169e1b43a22d75a2d98605e1
Robarto <robartpetrov@gmail.com>
2023-05-22 19:41:32 +0300

    added static files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 
 const PORT = 5000;
+
+// express config
+app.use(express.static(path.resolve(__dirname, "public")));
 
 // handlebars config
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
@@ -13,7 +17,7 @@ app.set("views", "src/views");
 
 // Routes
 app.get("/", (req, res) => {
-  res.render('index')
+  res.render("index");
 });
 
 app.listen(PORT);

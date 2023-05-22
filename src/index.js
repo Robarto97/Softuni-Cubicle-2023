6acb2b99f09c2021e1e2010e7b5e3017caac9227
Robarto <robartpetrov@gmail.com>
2023-05-22 19:31:54 +0300

    added handlebars

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+const app = express();
+
+const PORT = 5000;
+
+// handlebars config
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render('index')
+});
+
+app.listen(PORT);

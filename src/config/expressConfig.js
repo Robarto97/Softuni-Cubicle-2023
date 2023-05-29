aebc7a3d43507dd687525ad801231811abcb711d
Robarto <robartpetrov@gmail.com>
2023-05-29 20:29:02 +0300

    connected to db

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const path = require("path");
 
 function expressConfig(app) {
   app.use(express.static("src/public"));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const expressConfig = require("./config/expressConfig");
 const handlebarsConfig = require("./config/handlebarsConfig");
+const dbConnect = require("./config/dbConfig");
 const routes = require("./routes");
 
 const app = express();
 
 const PORT = 5000;
+
+dbConnect()
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log("Db error:", err));
 
 expressConfig(app);
 handlebarsConfig(app);
